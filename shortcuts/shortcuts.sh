a00ae1e9b2ce1b4fdec33560155348a3e258434d
C9-HP <7039985@gmail.com>
2024-06-29 13:00:04 +0330

    Add shortcuts: keep-screen-on/off

--- shortcuts/shortcuts.sh
+++ shortcuts/shortcuts.sh
@@ -1,1 +1,3 @@
-alias sync-obsidian=
+alias keep-screen-on="sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target"
+alias keep-screen-off="sudo systemctl unmask sleep.target suspend.target hibernate.target hybrid-sleep.target"
+
